61c9da9db6d80dfa9ce6c909d511915d3f3e0d68
Razziel <razziel496@gmail.com>
2019-06-26 14:31:29 +0200

    Styling

--- src/app/add-task/add-task.component.ts
+++ src/app/add-task/add-task.component.ts
@@ -1,20 +1,19 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-add-todo',
-  templateUrl: './add-todo.component.html',
-  styleUrls: ['./add-todo.component.scss']
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.scss']
 })
-export class AddTodoComponent implements OnInit {
+export class AddTaskComponent implements OnInit {
   @Output() addTodo: EventEmitter<any> = new EventEmitter();
 
   title: string
   constructor() { }
 
   ngOnInit() {
-  }a
+  }
 
-  //It adds todo on the list
   onSubmit() {
     const todo = {
       title: this.title,
@@ -24,7 +23,7 @@ export class AddTodoComponent implements OnInit {
       this.addTodo.emit(todo);
     }
     else {
-      window.alert('Wrong title. Try again.')
+      window.alert('Wrong title of todo. Please try again.')
     }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AddTaskComponent } from './add-task/add-task.component';
@@ -16,7 +18,9 @@ import { TodoListComponent } from './todo-list/todo-list.component';
     TodoListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
