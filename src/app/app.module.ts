3271d376009d4598a1e3f398e1a77e8679e6c2f6
Razziel <razziel496@gmail.com>
2019-06-26 14:01:03 +0200

    header created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AddTaskComponent } from './add-task/add-task.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddTaskComponent
+    AddTaskComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
